460fde44eece4657d9cfb35b0503cf2c5f7c7406
Aalekh Modgil <aalekhmodgil8@gmail.com>
2024-12-10 14:38:01 +0530

    [Aalekh] ADD : Prompt for a valid integer between 1 and 10

---
+++ Q3.rb
@@ -0,0 +1,11 @@
+loop do
+  print "Enter a number between 1 and 10 : "
+  number = gets.to_i
+  if number < 1 || number > 10
+    puts "Invalid number!"
+    redo
+  else
+    puts "Number is : #{number}"
+    break
+  end
+end
