54a1b51f72a906dded4fbff416ffd74fd6a2cfe1
Aalekh Modgil <aalekhmodgil8@gmail.com>
2024-12-11 10:37:05 +0530

    Raise and handle a custom age error for invalid age range input

---
+++ Exception/Prob2.rb
@@ -0,0 +1,17 @@
+class CustomAgeError < StandardError
+  def initialize(msg = "Age must be between 18 and 100")
+    super(msg)
+  end
+end
+
+begin
+  print "Enter your age :- "
+  age = gets.to_i
+  raise CustomAgeError if  age < 0 || age > 100
+  puts "Your age is :- #{age}"
+rescue CustomAgeError => e
+  puts "Age not valid! Please try again."
+  retry
+end
+
+
